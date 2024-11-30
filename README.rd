da85402d4902bc54ce046351b89465fb9250f0a1
Ernesto Quito Gonzales <quito.gonz.ern@gmail.com>
2024-11-30 10:21:07 -0500

    cambios variados

--- README.rd
+++ README.rd
@@ -1,1 +1,6 @@
-1. 
+1. Registrar tareas diarias
+2. Guardar registros de tareas en un archivo con extensión .log (primer release).
+3. Indicar fecha hora minuto y segundo de inicio con el comentario de la tarea.
+4. Indicar fecha hora minuto y segundo de fin de la tarea.
+5. Indicar el tiempo en formato HH:MM:SS del tiempo transcurrido por cada tarea.
+6. Si una tarea iniciada es interrumpida por otra tarea. La tarea debe tener fecha de fin con el tiempo empleado e iniciar una nueva linea o tarea.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::io::{self, Write};
+use chrono::prelude::*;
+use std::thread;
+use std::time::Duration;
+
+fn saludos() {
+    let saludo = "## BIENVENIDO AL REGISTRO DE TAREAS ##";
+    let michi = "#";
+    let repeat_michi = michi.repeat(saludo.len());
+
+    println!();
+    println!("{}", repeat_michi);
+    println!("{}", saludo);
+    println!("{}", repeat_michi);
+}
+
+fn nombre_tarea() -> String {
+    let mut tarea = String::new();
+
+    loop {
+        print!("Add task: ");
+        io::stdout().flush().unwrap();
+
+        io::stdin()
+            .read_line(&mut tarea)
+            .unwrap();
+
+        if tarea.trim().is_empty() {
+            println!("Task not can be empty. Please train again.")
+        } else {
+            return tarea;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    saludos();
+
+    let task = nombre_tarea();
+    let start_task= Local::now();
+    print!("INICIO={}={}", start_task, task.trim());
+    io::stdout().flush().unwrap();
+
+    thread::sleep(Duration::new(4, 0));
+
+    let end_task = Local::now();
+    println!(" - Fin: {}", end_task);
 }
